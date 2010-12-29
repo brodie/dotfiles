3b1cfdb00b6546a051ef9a82ae9ef93f606651d4
Brodie Rao <brodie@bitheap.org>
2010-12-29 14:15:44 +1100

    pythonrc: remove pprompt (doesn't play well with readline, even with its ignore markers)

--- .pythonrc.py
+++ .pythonrc.py
@@ -184,16 +184,6 @@ def _pythonrc_enable_pprint():
 
     sys.displayhook = pprinthook
 
-def _pythonrc_enable_pprompt():
-    """Colorize PS1 and PS2"""
-    import os
-    import sys
-
-    if (sys.platform != 'win32' and 'TERM' in os.environ and
-        os.environ['TERM'] != 'dumb'):
-        sys.ps1 = '\033[0;32m>>>\033[0m '
-        sys.ps2 = '\033[0;30m...\033[0m '
-
 def _pythonrc_fix_linecache():
     """Add source(obj) that shows the source code for a given object"""
     import os
@@ -379,11 +369,9 @@ if __name__ == '__main__':
         try:
             _pythonrc_enable_readline()
             _pythonrc_enable_pprint()
-            _pythonrc_enable_pprompt()
             _pythonrc_fix_linecache()
             del _pythonrc_enable_readline
             del _pythonrc_enable_pprint
-            del _pythonrc_enable_pprompt
             del _pythonrc_fix_linecache
         finally:
             if cwd is not None:
